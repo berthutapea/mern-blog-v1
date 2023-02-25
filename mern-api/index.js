0ec35741e112b6baefd03d4522bebf552cc0dbf9
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-26 05:36:45 +0700

    MERN Apo

--- mern-api/index.js
+++ mern-api/index.js
@@ -1,11 +1,26 @@
 const express = require('express');
 
 const app = express();
+const router = express.Router();
 
-app.use(() => {
-    console.log('Hello Server');
-    console.log('Hello Server');
-    console.log('Hello Server');
+router.use('/products', (req, res, next) => {
+    res.json({ name: "Gilbert Hutapea", email: "hutapeabert2002@gmail.com" })
+    next();
 })
+
+router.use('/price', (req, res, next) => {
+    res.json({ price: 3000000 });
+    next();
+})
+
+router.get('/customers', (req, res, next) => {
+    res.json({ title: "Costomers" });
+    next();
+})
+
+app.use('/', router);
+
+
+// GET '/users'
 
 app.listen(4000)
