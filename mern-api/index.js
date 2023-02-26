9a3405e1a5f82a45553c6a8aadfb329f5eab1fa8
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-26 19:08:03 +0700

    res.setHeader

--- mern-api/index.js
+++ mern-api/index.js
@@ -6,7 +6,7 @@ const productRoutes = require('./src/routes/products');
 app.use((req, res, next) => {
     // res.setHeader('Access-Control-Allow-Origin', '*');
     // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    // res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 })
 
