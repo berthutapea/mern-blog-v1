3f1c2f804fa21b6a09b1713cb75655e42bc4840b
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-27 17:10:31 +0700

    Request Body

--- mern-api/src/controllers/products.js
+++ mern-api/src/controllers/products.js
@@ -14,18 +14,18 @@ exports.createProduct = (req, res, next) => {
     next();
 }
 
-// exports.getAllProducts = (req, res, next) => {
-//     res.json(
-//         {
-//             message: 'Get All Products Succses',
-//             data: [
-//                 {
-//                     id: 1,
-//                     name: 'Sari Gandum',
-//                     price: 8000
-//                 }
-//             ]
-//         }
-//     );
-//     next();
-// }
+exports.getAllProducts = (req, res, next) => {
+    res.json(
+        {
+            message: 'Get All Products Succses',
+            data: [
+                {
+                    id: 1,
+                    name: 'Sari Gandum',
+                    price: 8000
+                }
+            ]
+        }
+    );
+    next();
+}
