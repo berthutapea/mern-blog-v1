29598d2007d735ed947779f5e0ba1f968bfa391f
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-27 20:08:39 +0700

    Endpoint Postingan Blog Baru (Create)

--- mern-api/src/routes/blog.js
+++ mern-api/src/routes/blog.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-// const blogController = require('../controllers/blog');
+const blogController = require('../controllers/blog');
 
 // [POST] : /v1/blog/post
 router.post('/post', blogController.createBlogPost);
