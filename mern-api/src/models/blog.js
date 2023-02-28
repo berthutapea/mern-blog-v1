2c098273293f13c97ae34e5b6620f2b1853fe747
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-28 17:14:46 +0700

    Setup MongoDB Model

---
+++ mern-api/src/models/blog.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const BlogPost = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    body: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: Object,
+        required: true,
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('BlogPost', BlogPost);
