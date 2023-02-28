6bac6ef51abe24dba8f239378a5b4f2a937c6081
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-28 16:59:21 +0700

    koneksi kan server MongoDB ke server Node JS

--- mern-api/index.js
+++ mern-api/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const authRoutes = require('./src/routes/auth');
@@ -22,6 +23,11 @@ app.use((error, req, res, next) => {
     const message = error.message;
     const data = error.data;
     res.status(status).json({ message: message, data: data });
-})
+});
 
-app.listen(4000)
+mongoose.connect('mongodb+srv://msnproduction:SZ1CMiHZyt8h1dcl@cluster0.nhyoubx.mongodb.net/?retryWrites=true&w=majority')
+    .then(() => {
+        app.listen(4000, () => console.log('Connection Success'));
+
+    })
+    .catch(err => console.log(err));
