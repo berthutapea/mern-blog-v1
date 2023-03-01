9fc7f4238cb774cfc75b4e403a6ce88f310fe5a3
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-03-01 20:49:04 +0700

    Text Area

--- mern-blog/src/components/atoms/TextArea/index.js
+++ mern-blog/src/components/atoms/TextArea/index.js
@@ -1,14 +1,14 @@
-// import React from 'react';
-// import './textArea.scss';
+import React from 'react';
+import './textArea.scss';
 
-// const TextArea = ({ ...rest }) => {
-//     return (
-//         <div>
-//             <textarea className="text-area" {...rest}>
+const TextArea = ({ ...rest }) => {
+    return (
+        <div>
+            <textarea className="text-area" {...rest}>
 
-//             </textarea>
-//         </div>
-//     )
-// }
+            </textarea>
+        </div>
+    )
+}
 
-// export default TextArea;
+export default TextArea;
