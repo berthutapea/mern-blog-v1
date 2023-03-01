63c2ce86ae562a289604a3893509f41f18ec49ec
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-03-01 20:46:04 +0700

    Button

--- mern-blog/src/components/atoms/Button/index.js
+++ mern-blog/src/components/atoms/Button/index.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import './button.scss';
+// import React from 'react'
+// import './button.scss';
 
-const Button = ({ title, ...rest }) => {
-    return <button className="button" {...rest}>{title}</button>
-}
+// const Button = ({ title, ...rest }) => {
+//     return <button className="button" {...rest}>{title}</button>
+// }
 
-export default Button;
+// export default Button;
