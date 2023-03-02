dbc47456081147389a7220cbc0cbecd308f744f1
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-03-02 13:45:22 +0700

    Merubah Value dari State Global Redux

--- mern-blog/src/config/redux/store.js
+++ mern-blog/src/config/redux/store.js
@@ -1,11 +1,23 @@
 const { createStore } = require("redux");
 
 const initialState = {
-    dataBlog: []
+    dataBlogs: [],
+    name: 'Gilbert'
 }
 
 const reducer = (state = initialState, action) => {
-
+    if(action.type === 'UPDATE_DATA_BLOG') {
+        return {
+            ...state,
+            dataBlogs: action.payload
+        }
+    }
+    if(action.type === 'UPDATE_NAME') {
+        return {
+            ...state,
+            name: 'Hutapea'
+        }
+    }
     return state;
 }
 
--- mern-blog/src/pages/Home/index.js
+++ mern-blog/src/pages/Home/index.js
@@ -3,20 +3,28 @@ import { BlogItem, Button, Gap } from '../../components';
 import './home.scss';
 import { useHistory } from 'react-router-dom';
 import Axios from 'axios';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Home = () => {
-  const [dataBlog, setDataBlog] = useState([]);
+  // const [dataBlog, setDataBlog] = useState([]);
+  const { dataBlogs, name } = useSelector(state => state)
+  const dispatch = useDispatch();
 
-  const stateGlobal = useSelector(state => state)
-  console.log('state global: ', stateGlobal);
+  // console.log('state global: ', stateGlobal);
+  console.log('data blog global: ', dataBlogs);
   useEffect(() => {
+    setTimeout(() => {
+      dispatch({ type: 'UPDATE_NAME' })
+    }, 3000)
+
+
     Axios.get('http://localhost:4000/v1/blog/posts?page=2&perPage=2')
       .then(result => {
         console.log('data Api', result.data);
         const responseAPI = result.data;
 
-        setDataBlog(responseAPI.data);
+        // setDataBlog(responseAPI.data);
+        dispatch({ type: 'UPDATE_DATA_BLOG', payload: responseAPI.data })
       })
       .catch(err => {
         console.log('error: ', err);
@@ -28,9 +36,10 @@ const Home = () => {
       <div className="create-wrapper">
         <Button title="create blog" onClick={() => history.push('/create-blog')} />
       </div>
+      <p>{name}</p>
       <Gap height={20} />
       <div className="content-wrapper">
-        {dataBlog.map(blog => {
+        {dataBlogs.map(blog => {
           return <BlogItem
             key={blog._id}
             image={`http://localhost:4000/${blog.image}`}
