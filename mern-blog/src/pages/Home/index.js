7bf3b4418d061d9c7cd1f4c1bfe4a9b0810e2e0f
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-03-02 11:37:27 +0700

    Sample Pemanggilan API Blog Post dengan menggunakan Axios

--- mern-blog/src/pages/Home/index.js
+++ mern-blog/src/pages/Home/index.js
@@ -1,19 +1,19 @@
-// import React, { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { BlogItem, Button, Gap } from '../../components';
 import './home.scss';
 import { useHistory } from 'react-router-dom';
-// import { Axios } from 'axios';
+import Axios from 'axios';
 
 const Home = () => {
-  // useEffect(() => {
-  //   Axios.get('http://localhost:4000/v1/blog/posts')
-  //     .then(result => {
-  //       console.log('data API', result);
-  //     })
-  //     .catch(err => {
-  //       console.log('error: ', err);
-  //     })
-  // }, [])
+  useEffect(() => {
+    Axios.get('http://localhost:4000/v1/blog/posts')
+      .then(result => {
+        console.log('data Api', result.data);
+      })
+      .catch(err => {
+        console.log('error: ', err);
+      })
+  }, [])
   const history = useHistory();
   return (
     <div className="home-page-wrapper">
