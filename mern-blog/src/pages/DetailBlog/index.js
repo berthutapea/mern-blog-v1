18684934e0b7587344da4852a1bdc94f2196a0fd
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-03-02 21:28:15 +0700

    Integrasi Detail Blog Post dengan unique ID

--- mern-blog/src/pages/DetailBlog/index.js
+++ mern-blog/src/pages/DetailBlog/index.js
@@ -22,7 +22,7 @@ const DetailBlog = (props) => {
             <div className="detail-blog-wrapper">
                 <img className="img-cover" src={`http://localhost:4000/${data.image}`} alt="thumb" />
                 <p className="blog-title">{data.title}</p>
-                {/* <p className="blog-author">{data.author.name} - {data.createdAt}</p> */}
+                <p className="blog-author">{data.author.name} - {data.createdAt}</p>
                 <p className="blog-body">{data.body}</p>
                 <Gap height={20} />
                 <Link title="Kembali Ke Home" onClick={() => history.push('/')} />
