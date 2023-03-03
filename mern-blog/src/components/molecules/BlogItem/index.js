ed59547bdcc888eec8e83f2f06885e01b6e5cff0
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-03-03 19:38:27 +0700

    Implementasi Delete Blog Post

--- mern-blog/src/components/molecules/BlogItem/index.js
+++ mern-blog/src/components/molecules/BlogItem/index.js
@@ -5,7 +5,7 @@ import { useHistory } from 'react-router-dom';
 
 const BlogItem = (props) => {
     const history = useHistory();
-    const { image, title, name, date, body, _id } = props;
+    const { image, title, name, date, body, _id, onDelete } = props;
     return (
         <div className="blog-item">
             <img className="image-thumb" src={image} alt="post" />
@@ -13,7 +13,7 @@ const BlogItem = (props) => {
                 <div className="title-wrapper">
                     <p className="title">{title}</p>
                     <div className="edit-wrapper">
-                        <p className="edit" onClick={() => history.push(`/create-blog/${_id}`)}>Edit</p> | <p className="delete">Delete</p>
+                        <p className="edit" onClick={() => history.push(`/create-blog/${_id}`)}>Edit</p> | <p className="delete" onClick={() => onDelete(_id)}>Delete</p>
                     </div>
                 </div>
                 <p className="author">{name} - {date}</p>
--- mern-blog/src/pages/Home/index.js
+++ mern-blog/src/pages/Home/index.js
@@ -4,6 +4,9 @@ import './home.scss';
 import { useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { setDataBlog } from '../../config/redux/action';
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css';
+import Axios from 'axios';
 
 const Home = () => {
   const [counter, setCounter] = useState(1);
@@ -24,6 +27,32 @@ const Home = () => {
     setCounter(counter === page.totalPage ? page.totalPage : counter + 1)
   }
 
+  const confirmDelete = (id) => {
+    confirmAlert({
+      title: 'Confirm to delete',
+      message: 'Apakah Anda Yakin Akan Menghapus Blog Ini?',
+      buttons: [
+        {
+          label: 'Ya',
+          onClick: () => {
+            Axios.delete(`http://localhost:4000/v1/blog/post/${id}`)
+              .then(res => {
+                console.log('success delete: ', res.data);
+                dispatch(setDataBlog(counter))
+              })
+              .catch(err => {
+                console.log('err: ', err);
+              })
+          }
+        },
+        {
+          label: 'Tidak',
+          onClick: () => console.log('user tidak setuju')
+        }
+      ]
+    });
+  }
+
   return (
     <div className="home-page-wrapper">
       <div className="create-wrapper">
@@ -40,6 +69,7 @@ const Home = () => {
             name={blog.author.name}
             date={blog.createdAt}
             _id={blog._id}
+            onDelete={confirmDelete}
           />
         })}
       </div>
