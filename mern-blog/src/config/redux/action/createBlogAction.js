edffc783926b095d70e6b8996809a2402f69a311
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-03-03 15:12:40 +0700

    Integrasi Detail Blog Post dengan unique ID

--- mern-blog/src/config/redux/action/createBlogAction.js
+++ mern-blog/src/config/redux/action/createBlogAction.js
@@ -4,9 +4,9 @@ export const setForm = (formType, formValue) => {
     return { type: 'SET_FORM_DATA', formType, formValue }
 }
 
-// export const setImgPreview = (payload) => {
-//     return { type: 'SET_IMG_PREVIEW', payload }
-// }
+export const setImgPreview = (payload) => {
+    return { type: 'SET_IMG_PREVIEW', payload }
+}
 
 export const postToApi = (form) => {
     const data = new FormData();
