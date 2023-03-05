37f8e74cc94f3ff924d90bca4f20ec864c1182ac
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-03-06 00:25:28 +0700

    AdminBlog

--- mern-blog/src/components/molecules/BlogAdmin/index.js
+++ mern-blog/src/components/molecules/BlogAdmin/index.js
@@ -9,26 +9,21 @@ const BlogAdmin = (props) => {
     const navigate = useNavigate();
     const { image, title, _id, onDelete } = props;
     return (
-        
-            <div className="panel panel-default">
-                <div className="panel-body">
-                    <table className="table-admin">
-                    <thead>
-                        <th>Title</th>
-                        <th>Image</th>
-                        <th>Action</th>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th className="admin-text-title">{title}</th>
-                            <td><img className="admin-img" src={image} alt="post" /></td>
-                            <td><p className="edit" onClick={() => navigate(`/create-blog/${_id}`)}><FaEdit /></p><p className="delete" onClick={() => onDelete(_id)}><FaTrash /></p></td>
-                        </tr>
-                    </tbody>
-                    </table>
-                </div>
-                <Gap height={20} />
-            </div>
+        <table className="table-admin">
+            <thead>
+                <th>Title</th>
+                <th>Image</th>
+                <th>Action</th>
+            </thead>
+            <tbody>
+                <tr>
+                    <th className="admin-text-title">{title}</th>
+                    <td><img className="admin-img" src={image} alt="post" /></td>
+                    <td><p className="edit" onClick={() => navigate(`/create-blog/${_id}`)}><FaEdit /></p><p className="delete" onClick={() => onDelete(_id)}><FaTrash /></p></td>
+                </tr>
+            </tbody>
+            <Gap height={20} />
+        </table>
     )
 }
 
