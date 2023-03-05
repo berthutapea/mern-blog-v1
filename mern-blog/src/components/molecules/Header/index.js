5d9368811d108b84612fad991d9c8ba1b9a210ab
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-03-05 18:44:53 +0700

    Header

--- mern-blog/src/components/molecules/Header/index.js
+++ mern-blog/src/components/molecules/Header/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './header.scss';
 import { useNavigate } from 'react-router-dom';
+import { LogoMsn } from '../../../assets';
 
 const Header = () => {
     const navigate = useNavigate();
     return (
         <div className="header">
-            <p className="logo-app">MERN-Blog</p>
+            <img className="logo-app" onClick={() => navigate('/')} src={LogoMsn} alt="logo" />
+            <p className="menu-item" onClick={() => navigate('/')}>Home</p>
+            <p className="menu-item" onClick={() => navigate('/create-blog')}>Create Blog</p>
             <p className="menu-item" onClick={() => navigate('/login')}>Logout</p>
         </div>
     )
