c2894f9ed65ae570993ef301447d445e551364ab
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-03-07 21:47:58 +0700

    Blog Admin

--- mern-blog/src/components/molecules/BlogAdmin/index.js
+++ mern-blog/src/components/molecules/BlogAdmin/index.js
@@ -5,28 +5,28 @@ import { useNavigate } from 'react-router-dom';
 import { FaTrash, FaEdit } from 'react-icons/fa';
 
 
-const BlogAdmin = (props) => {
-    const navigate = useNavigate();
-    const { image, title, _id, onDelete } = props;
-    return (
-        <>
-            <table className="table-admin">
-                <thead>
-                    <th>Title</th>
-                    <th>Image</th>
-                    <th>Action</th>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th className="admin-text-title">{title}</th>
-                        <td><img className="admin-img" src={image} alt="post" /></td>
-                        <td><p className="edit" onClick={() => navigate(`/create-blog/${_id}`)}><FaEdit /></p><p className="delete" onClick={() => onDelete(_id)}><FaTrash /></p></td>
-                    </tr>
-                </tbody>
-                <Gap height={20} />
-            </table>
-        </>
-    )
-}
+// const BlogAdmin = (props) => {
+//     const navigate = useNavigate();
+//     const { image, title, _id, onDelete } = props;
+//     return (
+//         <>
+//             <table className="table-admin">
+//                 <thead>
+//                     <th>Title</th>
+//                     <th>Image</th>
+//                     <th>Action</th>
+//                 </thead>
+//                 <tbody>
+//                     <tr>
+//                         <th className="admin-text-title">{title}</th>
+//                         <td><img className="admin-img" src={image} alt="post" /></td>
+//                         <td><p className="edit" onClick={() => navigate(`/create-blog/${_id}`)}><FaEdit /></p><p className="delete" onClick={() => onDelete(_id)}><FaTrash /></p></td>
+//                     </tr>
+//                 </tbody>
+//                 <Gap height={20} />
+//             </table>
+//         </>
+//     )
+// }
 
 export default BlogAdmin;
