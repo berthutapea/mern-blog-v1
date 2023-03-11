1533e031c42fff6b538152a38174b0f51c9f5b1e
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-03-11 19:58:06 +0700

    Register

--- mern-blog/src/pages/Register/index.js
+++ mern-blog/src/pages/Register/index.js
@@ -1,30 +1,30 @@
-// import React from 'react';
-// import './register.scss';
-// import { RegisterBg } from '../../assets';
-// import { Button, Input, Gap, Link } from '../../components';
-// import { useNavigate } from 'react-router-dom';
+import React from 'react';
+import './register.scss';
+import { RegisterBg } from '../../assets';
+import { Button, Input, Gap, Link } from '../../components';
+import { useNavigate } from 'react-router-dom';
 
-// const Register = () => {
-//   const navigate = useNavigate();
-//   return (
-//     <div className="main-page">
-//       <div className="left">
-//         <img src={RegisterBg} className="bg-image" alt="imageBg" />
-//       </div>
-//       <div className="right">
-//         <p className="title">Register</p>
-//         <Input label="Ful Name" placeholder="Full Name" />
-//         <Gap height={18} />
-//         <Input label="Email" placeholder="Email" />
-//         <Gap height={18} />
-//         <Input label="Password" placeholder="Password" />
-//         <Gap height={50} />
-//         <Button title="Register" onClick={() => navigate('/login')} />
-//         <Gap height={100} />
-//         <Link title="Kembali ke Login" onClick={() => navigate('/login')} />
-//       </div>
-//     </div>
-//   )
-// }
+const Register = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="main-page">
+      <div className="left">
+        <img src={RegisterBg} className="bg-image" alt="imageBg" />
+      </div>
+      <div className="right">
+        <p className="title">Register</p>
+        <Input label="Ful Name" placeholder="Full Name" />
+        <Gap height={18} />
+        <Input label="Email" placeholder="Email" />
+        <Gap height={18} />
+        <Input label="Password" placeholder="Password" />
+        <Gap height={50} />
+        <Button title="Register" onClick={() => navigate('/login')} />
+        <Gap height={100} />
+        <Link title="Kembali ke Login" onClick={() => navigate('/login')} />
+      </div>
+    </div>
+  )
+}
 
-// export default Register;
+export default Register;
