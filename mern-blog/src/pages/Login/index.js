8d639abd9f76589ab6c9d2215e3a9b29a5516666
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-03-12 20:24:58 +0700

    Action

--- mern-blog/src/pages/Login/index.js
+++ mern-blog/src/pages/Login/index.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import { LoginBg } from '../../assets';
-import { Button, Input, Gap, Link } from '../../components';
-import { useNavigate } from 'react-router-dom';
+// import React from 'react';
+// import { LoginBg } from '../../assets';
+// import { Button, Input, Gap, Link } from '../../components';
+// import { useNavigate } from 'react-router-dom';
 
 
-const Login = () => {
-  const navigate = useNavigate();
-  return (
-    <div className="main-page">
-      <div className="left">
-        <img src={LoginBg} className="bg-image" alt="imageBg" />
-      </div>
-      <div className="right">
-        <p className="title">Login</p>
-        <Input label="Email" placeholder="Email" />
-        <Gap height={18} />
-        <Input label="Password" placeholder="Password" />
-        <Gap height={50} />
-        <Button title="Login" onClick={() => navigate('/')} />
-        <Gap height={100} />
-        <Link title="Belum Punya Akun? Silahkan Daftar!" onClick={() => navigate('/register')} />
-      </div>
-    </div>
-  )
-}
+// const Login = () => {
+//   const navigate = useNavigate();
+//   return (
+//     <div className="main-page">
+//       <div className="left">
+//         <img src={LoginBg} className="bg-image" alt="imageBg" />
+//       </div>
+//       <div className="right">
+//         <p className="title">Login</p>
+//         <Input label="Email" placeholder="Email" />
+//         <Gap height={18} />
+//         <Input label="Password" placeholder="Password" />
+//         <Gap height={50} />
+//         <Button title="Login" onClick={() => navigate('/')} />
+//         <Gap height={100} />
+//         <Link title="Belum Punya Akun? Silahkan Daftar!" onClick={() => navigate('/register')} />
+//       </div>
+//     </div>
+//   )
+// }
 
-export default Login;
+// export default Login;
