71030c8b49e05fcdce44079bd0684d505ec30b00
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-03-13 19:54:07 +0700

    Search

--- mern-blog/src/components/molecules/SearchBlog/index.js
+++ mern-blog/src/components/molecules/SearchBlog/index.js
@@ -1,34 +1,34 @@
-// import React, { useState } from 'react';
-// import axios from 'axios';
-// import './searchBlog.scss';
+import React, { useState } from 'react';
+import axios from 'axios';
+import './searchBlog.scss';
 
-// function SearchBlog() {
-//   const [searchTerm, setSearchTerm] = useState('');
-//   const [searchResults, setSearchResults] = useState([]);
+function SearchBlog() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
 
-//   const handleSearch = async event => {
-//     const term = event.target.value.toLowerCase();
-//     setSearchTerm(term);
-//     try {
-//       const response = await axios.get(`http://localhost:4000/v1/blog/posts?search=${term}`);
-//       const results = response.data;
-//       setSearchResults(results);
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   };
+  const handleSearch = async event => {
+    const term = event.target.value.toLowerCase();
+    setSearchTerm(term);
+    try {
+      const response = await axios.get(`http://localhost:4000/v1/blog/posts?search=${term}`);
+      const results = response.data;
+      setSearchResults(results);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-//   return (
-//     <div>
-//       <input type="text" value={searchTerm} onChange={handleSearch} />
-//       {Array.isArray(searchResults) && searchResults.length > 0 && searchResults.map((blog) => (
-//         <div key={blog}>
-//           <p>{blog.title}</p>
-//           <p>{blog.content}</p>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
+  return (
+    <div>
+      <input type="text" value={searchTerm} onChange={handleSearch} />
+      {Array.isArray(searchResults) && searchResults.length > 0 && searchResults.map((blog) => (
+        <div key={blog}>
+          <p>{blog.title}</p>
+          <p>{blog.content}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
 
-// export default SearchBlog;
+export default SearchBlog;
